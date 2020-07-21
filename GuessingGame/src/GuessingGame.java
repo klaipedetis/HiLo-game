6770d2227cc7d8991cbce709dc89f9aecc8649cc
klaipedetis <rshabashov@yandex.ru>
2020-07-21 04:22:58 +0100

    added label and button: guess

--- GuessingGame/src/GuessingGame.java
+++ GuessingGame/src/GuessingGame.java
@@ -1,7 +1,26 @@
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class GuessingGame extends JFrame {
 	public GuessingGame() {
+		
+		JButton btnGuess = new JButton("Guess");
+		btnGuess.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnGuess.setFont(new Font("Dialog", Font.BOLD, 14));
+		getContentPane().add(btnGuess, BorderLayout.CENTER);
+		
+		JLabel lblNewLabel = new JLabel("Guessing Game");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		getContentPane().add(lblNewLabel, BorderLayout.NORTH);
 	}
 
 	/**
@@ -13,5 +32,4 @@ public class GuessingGame extends JFrame {
 		// TODO Auto-generated method stub
 
 	}
-
 }
