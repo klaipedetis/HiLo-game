31a7253d010ef5b2b8fdc98eef02f43ea2265715
klaipedetis <rshabashov@yandex.ru>
2020-07-21 04:10:03 +0100

    Create basic class for HiLo game GUI

---
+++ GuessingGame/src/GuessingGame.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class GuessingGame extends JFrame {
+	public GuessingGame() {
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
